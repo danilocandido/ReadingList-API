6da14fdd730e8ffe54d3fedc75f19b9d2deef6dc
Danilo Cândido <danilod07@gmail.com>
2018-02-01 16:49:14 -0300

    creating books -> post

---
+++ test/integration/creating_books_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class CreatingBooksTest < ActionDispatch::IntegrationTest
+  test 'create new books' do
+    post '/books', params: { book: { title: 'Livro do Danilo', rating: 5 } }
+    assert_equal 201, response.status
+    book = json(response.body)
+    assert_equal book_url(book[:id]), response.location
+  end
+end
