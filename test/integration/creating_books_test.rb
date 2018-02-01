65ea41dc36b0bd9e435308d8c80a4a284ab61da6
Danilo Cândido <danilod07@gmail.com>
2018-02-01 16:54:10 -0300

    creating test invalid descriptioon

--- test/integration/creating_books_test.rb
+++ test/integration/creating_books_test.rb
@@ -1,11 +1,15 @@
 require 'test_helper'
 
 class CreatingBooksTest < ActionDispatch::IntegrationTest
-  test 'create new books' do
+  test 'create new books with valid data' do
     post '/books', params: { book: { title: 'Livro do Danilo', rating: 5 } }
     assert_equal 201, response.status
     book = json(response.body)
     assert_equal book_url(book[:id]), response.location
     assert_equal 'Livro do Danilo', book[:title]
+  end
+
+  test 'does not create books with invalid data' do
+    
   end
 end
