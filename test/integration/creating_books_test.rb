f4bf7211a0878a2263a11ae8cc5aed1a4c83bb36
Danilo Cândido <danilod07@gmail.com>
2018-02-01 16:53:03 -0300

    verifying book title

--- test/integration/creating_books_test.rb
+++ test/integration/creating_books_test.rb
@@ -6,5 +6,6 @@ class CreatingBooksTest < ActionDispatch::IntegrationTest
     assert_equal 201, response.status
     book = json(response.body)
     assert_equal book_url(book[:id]), response.location
+    assert_equal 'Livro do Danilo', book[:title]
   end
 end
